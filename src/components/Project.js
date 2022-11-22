791a48e60bd389a65e61b9998d8f48abca8314b5
melissa <melissastan91@gmail.com>
2022-11-21 22:51:09 -0500

    cards rendering works

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,60 @@
 import React from "react";
+import ProjectCard from './ProjectCard';
+import projectOnePic from '../assets/project1pic.png';
+import projectTwoPic from '../assets/project2pic.png';
+import projectThreePic from '../assets/comingsoon.jpg';
 
-function Project(props) {
+
+// import profilePic from '../assets/profilePic.JPG';
+
+const projects = [
+  {
+      title: '1',
+      description:
+          'x',
+      image: projectOnePic,
+      technologies: [
+          'x',
+      ],
+      github: 'x',
+      deployedLink: 'x',  
+  },
+  {
+      title: '2',
+      description:
+        'y',
+      image: projectTwoPic,
+      technologies: [
+        'y',
+      ],
+      github: 'y',
+      deployedLink: 'y',
+},
+{
+  title: '3',
+  description:
+    'y',
+  image: projectThreePic,
+  technologies: [
+    'y',
+  ],
+  github: 'y',
+  deployedLink: 'y',
+}
+]
+
+export default function Project() {
+  let allProjects = [];
+  for (var i = 0; i < projects.length; i++) {
+    allProjects.push( <ProjectCard key={projects[i].title} image={projects[i].image} name={projects[i].title} description={projects[i].description} technologies={projects[i].technologies} deployedLink={projects[i].deployedLink} />);
+  }
   return (
-    <div className="project text-center">
-      <div className="project-header">
+    <div>
+      <div className="project-header card">
         <h2>Projects</h2>
       </div>
-      <div className="project-body">{props.children}</div>
+  
+      {allProjects}
     </div>
   );
 }
-
-export default Project;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import '../styles/Resume.css';
 
+<i class="fa-regular fa-download"></i>
 function Resume() {
   return (
     <div className="resume text-center">
       <div className="resume-header">
         <h2>Resumé</h2>
       </div>
-      {/* <div className="resume-body">{props.children}</div> */}
+      <a href="https://github.com/mstan19"> <FontAwesomeIcon icon={faDownload} className='icon px-3' id='downloadIcon'/> </a>
     </div>
   );
 }
